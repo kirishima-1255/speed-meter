f3422c01e17b293b4c8ddfabc993e2a317bbbda9
kirishima-1255 <kirishima1255@gmail.com>
2024-12-06 14:22:00 +0900

    ドライブモード表示機能実装

--- main.js
+++ main.js
@@ -26,15 +26,60 @@ async function lamp_change(type, status) {
     }
 }
 
+async function drivemde_change(status) {
+    if (status === "P") {
+        let drivemode = document.getElementById('drivemode_lamp');
+        drivemode.textContent = "P"; 
+    } else if (status === "N") {
+        let drivemode = document.getElementById('drivemode_lamp');
+        drivemode.textContent = "N"; 
+    } else if (status === "R") {
+        let drivemode = document.getElementById('drivemode_lamp');
+        drivemode.textContent = "R";
+    } else if (status === "D") {
+        let drivemode = document.getElementById('drivemode_lamp');        
+        drivemode.textContent = "D";
+    } else {
+        lamp_change("EPB",1)
+    }
+}
+
 
 
 
 async function bootlo(){
+    //初期設定：消灯
+    lamp_change("ecomode",0)
     lamp_change("abs",0)
-    await sleep(1000)
+    lamp_change("battery",0)
+    lamp_change("door",0)
+    lamp_change("brake",0)
+    lamp_change("EPB",0)
+    lamp_change("lowbeam",0)
+    lamp_change("highbeam",0)
+    lamp_change("signalL",0)
+    lamp_change("signalR",0)
+    await sleep(450)
+    //チェック用に点灯
     lamp_change("abs",1)
+    await sleep(80)
+    lamp_change("battery",1)
+    await sleep(50)
+    lamp_change("brake",1)
+    await sleep(70)
+    lamp_change("door",1)
+    await sleep(90)
+    lamp_change("EPB",1)
+    await sleep(110)
+    drivemde_change("P")
     await sleep(1000)
+    //チェック後消灯
+
     lamp_change("abs",0)
+    lamp_change("battery",0)
+    lamp_change("brake",0)
+    lamp_change("door",0)
+    lamp_change("EPB",0)
 }
 
 bootlo()
