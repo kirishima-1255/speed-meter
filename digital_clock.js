092862a91443ff6a90ec2081d8a693bc400aac04
kirishima-1255 <kirishima1255@gmail.com>
2024-12-07 17:03:13 +0900

    clck

--- digital_clock.js
+++ digital_clock.js
@@ -1,3 +1,12 @@
+function twoDigit(num) {
+    let ret;
+    if( num < 10 ) 
+        ret = "0" + num; 
+    else 
+        ret = num; 
+    return ret;
+  }
+
 let nowTime = new Date(); //  現在日時を得る
 let nowHour = twoDigit( nowTime.getHours() );
 let nowMin  = twoDigit( nowTime.getMinutes() );
