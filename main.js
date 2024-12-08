b176a469996e2997f5bbeec502f681e14ce3a300
Kirishima <kirishima1255@gmail.com>
2024-12-09 02:46:20 +0900

    js

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ async function winker(){
     await sleep(385)
     lamp_change("signalL",0)
     lamp_change("signalR",0)
-    await sleep(385)
+    await sleep(340)
 }
 
 async function winker_sound(){
